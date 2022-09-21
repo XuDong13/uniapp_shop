7510caeed05888aff785c960dbeefe21fd42eb55
WXD020217 <1334929644@qq.com>
2022-09-21 11:20:20 +0800

    完成商品列表功能

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,72 @@
+<template>
+  <view class="goods-item">
+    <!-- 左侧 -->
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo||defaultPic" class="goods-img"></image>
+    </view>
+    <!-- 右侧 -->
+    <view class="goods-item-right">
+      <view class="goods-title">
+        {{goods.goods_name}}
+      </view>
+      <view class="goods-price">
+        ￥{{goods.goods_price | tofixed}}
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    filters: {
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px;
+    background-color: #fff;
+    border-bottom: 1px solid #ccc;
+
+    .goods-item-left {
+      margin-right: 8px;
+
+      .goods-img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-title {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        color: #c00000;
+        font-size: 16px;
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
-  <view>
-    goodsList
+  <view class="goods-list">
+    <view v-for="(item,index) in goodsList" :key="index" @click="gotoGoodsDetail(item)">
+      <my-goods :goods="item"></my-goods>
+    </view>
   </view>
 </template>
 
@@ -8,8 +10,54 @@
   export default {
     data() {
       return {
-
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isloading: false
       };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList(callback) {
+        this.isloading = true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+        this.isloading = false
+        callback && callback()
+      },
+      gotoGoodsDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('没有更多数据了哦')
+      if (this.isloading) return
+      this.queryObj.pagenum++
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1
+      this.goodsList = []
+      this.total = 0
+      this.isloading = false
+      this.getGoodsList(() => {
+        uni.stopPullDownRefresh()
+      })
     }
   }
 </script>
